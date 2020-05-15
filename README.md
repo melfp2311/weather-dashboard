b30ff81adbc8068c4246314769ec570f85680641
Melissa Flores <mel.floresp@gmail.com>
2020-05-14 22:48:17 -0500

    first commitment

---
+++ README.md
@@ -0,0 +1,76 @@
+# TDM coding bootcamp
+# Melissa Flores
+# 06 Server-Side APIs: Weather Dashboard
+
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+
+Used the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. 
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+WHEN I open the weather dashboard
+THEN I am presented with the last searched city forecast
+```
+
+The following image demonstrates the application functionality:
+
+![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+
+## Review
+
+You are required to submit the following for review:
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+## Tech and Features Used
+
+Bootstrap
+Javascript
+JQuery
+Open Weather API
+Postman
+​
+
+How to use
+​Type in a cities name in the search bar and click search. on the right side of the screen you will see the current weather on a large card. underneath that card you will see the five day forecast. After you click search, a button will appear on the left side of the screen underneath the search bar with the name of the city that you just searched for. When you click on that button the right side of the screen will populate the weather information for that city. When you search for a new city the search button will generate a new history button underneath the search bar, and the old search button will move down a level. Everytime you search for a new city the same process will take place until you have reached five cities and then the oldest city button will dissapear.​
+
+Technical Overview
+​
+
+The search button triggers the ajax call to the open weather current weather api, the uv index api, the five day forecast weather api, and saves the city name to a variable,saves the city name to an array in local storage and if there is any information populated on any of the weather cards it clears that information.
+The openweather api will send back the weather information including the latitude and longitude for the city we are searching for.
+The uv index api is going to use the latitude and longitude information from the current weather api to give us the uv index.
+the city name variable is going to be used in the ajax call for the 5 day forecast api.
+the city name will be saved in local storage.
+there is a function that is called before the ajax calls that takes the saved information from local storage and loops through the city list array and creates a list of buttons with the name of the city on each button.
+the button will go through the same process of the seach button with the exception of saving the city name to the local storage array, and therefore does not create a new city button under the search bar.
+As the API calls finish the information needed (temperature, humidity, uv index and windspeed) populate the current weather card, as well as the five day forecast cards.
+Challenges
+Some of the challenges I faced:
+
+the JQuery click method did not like working on dynamically created classes, I learned that click methods need to be grabbed from the static parent of the created buttons. with the syntax being $("#city-list").on("click", "button",function()) with the selector being city-list, then the event handler, then the event selector being button.
+
+the other challenge i had were the ajax calls being synchronous and how or if I could wait for the information i was getting would affect the next multiple ajax calls. The uv index call required lon and lat information which came from the current weather api. so I nested the uv index ajax call in the current weather call.
+
+the five day forecast also needed the city ID code, if I wanted to make sure that the same city that was being called from the current weather api, so I nested that in the .then method after the ajax call as well so to wait for the information to get back. ultimately i did not have to do any async manipulation with this app.​L
---
+++ script.js
@@ -0,0 +1,257 @@
+var citiesListEl = $("#city-list")
+var cities = []
+var cityName, fiveDayQueryURL, weatherData, currentWeatherIcon, currentWeatherIconEl, weathericon, currentTemp, weatherCard, cityDateEl, tempEl, humidityEl, windspeedEl, fiveDayQueryParams, fiveDayList;
+
+
+function TodayQueryUrl() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+    var queryParams = { "appid": "0d2a570544db7d02e47387057bd868ca" };
+    queryParams.q = $("#search-term").val().trim();
+    queryParams.units = "imperial"
+    return queryURL + $.param(queryParams);
+}
+
+
+function FiveDayQueryUrl() {
+    var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+    var fiveDayQueryParams = { "appid": "0d2a570544db7d02e47387057bd868ca" };
+    fiveDayQueryParams.id = data.id;
+    fiveDayQueryParams.units = "imperial";
+    return fiveDayQueryURL + $.param(fiveDayQueryParams);
+}
+
+
+//To show saved cities and user input
+init();
+function renderCities() {
+    if (cities.length > 5) {
+        cities.shift();
+    }
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+        var li = $("<li>")
+        var button = $("<button>");
+        button.text(city);
+        // button.attr("data-index", i);
+        // button.attr("style", "width: 100%")
+        button.addClass("btn shadow-box btn-info hist-button");
+        li.append(button);
+        $("#city-list").prepend(li);
+        $("#city-list").prepend("<br>");
+    }
+}
+
+//Local Storage for cities
+function init() {
+    $("#city-list").empty();
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    if (storedCities !== null) {
+        cities = storedCities;
+    }
+    renderCities();
+}
+
+
+//Event " click" listener
+$(".search-button").on("click", function (event) {
+    event.preventDefault();
+    $("#current-day-forecast").empty();
+    $("#five-day-forecast").empty();
+    var searchHistory = $("#search-term").val().trim();
+    if (searchHistory === "") {
+        return;
+    };
+    cities.push(searchHistory)
+    localStorage.setItem("cities", JSON.stringify(cities));
+    queryURL = TodayQueryUrl();
+    var fiveDayQueryURL;
+
+// ---------------------Beginning of AJAX Call---------------------------
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (data) {
+        function CurrentWeatherCard() {
+            $(weatherCard).append(cityDateEl);
+            $(weatherCard).append(weathericon)
+            $(weatherCard).append(tempEl);
+            $(weatherCard).append(humidityEl);
+            $(weatherCard).append(windspeedEl);
+            $("#current-day-forecast").append(weatherCard);
+        }
+        var date = moment().format("MMM Do YY");
+        var weatherData = data;
+        var currentWeatherIcon = data.weather[0].icon;
+        var currentWeatherIconEl = "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png";
+        var weathericon = $("<img/>", {
+            id: "weather-icon",
+            src: currentWeatherIconEl,
+            width: 75
+        });
+        var currentTemp = Math.floor(weatherData.main.temp);
+        var weatherCard = $("<div>").addClass("card weather-card current-day-weather");
+        var cityDateEl = $("<h5>").addClass("card-title").text(weatherData.name + " " + "(" + date + ")");
+        var tempEl = $("<p>").addClass("card-text").text("Temp: " + currentTemp + " F");
+        var humidityEl = $("<p>").addClass("card-text text-nowrap").text("Humidity: " + weatherData.main.humidity + " %");
+        var windspeedEl = $("<p>").addClass("card-text").text("Windspeed: " + weatherData.wind.speed + " mph");
+        CurrentWeatherCard();
+        function FiveDayQueryUrl() {
+            var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+            var fiveDayQueryParams = { "appid": "0d2a570544db7d02e47387057bd868ca" };
+            fiveDayQueryParams.id = data.id;
+            fiveDayQueryParams.units = "imperial";
+            return fiveDayQueryURL + $.param(fiveDayQueryParams);
+        }
+        fiveDayQueryURL = FiveDayQueryUrl();
+
+
+// five day forecast ajax call
+        $.ajax({
+            url: fiveDayQueryURL,
+            method: "GET"
+        }).then(function (fiveData) {
+            fiveDayList = fiveData.list;
+            for (var i = 4; i < fiveDayList.length; i += 8) {
+                var day = fiveDayList[i];
+                var dayIcon = day.weather[0].icon;
+                var dayWeatherIcon = "https://openweathermap.org/img/wn/" + dayIcon + ".png";
+                var dayIconEl = $("<img/>", {
+                    id: "weather-icon",
+                    src: dayWeatherIcon,
+                    width: 50
+                })
+                var dayTempEl = Math.floor(day.main.temp);
+                var dayCard = $("<div>").addClass("card weather-card col-lg bg-info text-white mr-md-2 mb-3");
+                var dayDate = $("<h5>").attr("style", "font-size:100%").addClass("card-title text-nowrap").text(moment().add(1, 'days').format('L'));
+                var dayTemp = $("<p>").addClass("card-text").text("Temp: " + dayTempEl + " F");
+                var dayHum = $("<p>").addClass("card-text text-nowrap").text("Humidity: " + day.main.humidity);
+                $(dayCard).append(dayDate);
+                $(dayCard).append(dayIconEl)
+                $(dayCard).append(dayTemp);
+                $(dayCard).append(dayHum);
+                $("#five-day-forecast").append(dayCard);
+            }
+        })
+        var uvIndexEl;
+        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=0d2a570544db7d02e47387057bd868ca"
+        CurrentWeatherCard();
+
+
+// uv index ajax call
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            uvIndexEl = response.value
+            uvIndexTag = $("<p>").text("UV Index: " + uvIndexEl)
+            $(".current-day-weather").append(uvIndexTag)
+        })
+        $("#search-term").val(null)
+        init();
+    });
+
+// ----------------END OF AJAX CALL-----------------------------
+
+});
+
+function QueryUrlHist() {
+    var queryURLHist = "https://api.openweathermap.org/data/2.5/weather?";
+    var queryParamsHist = { "appid": "0d2a570544db7d02e47387057bd868ca" };
+    queryParamsHist.q = cityName;
+    queryParamsHist.units = "imperial"
+    return queryURLHist + $.param(queryParamsHist);
+};
+function CurrentWeatherCardHist() {
+    $(weatherCard).append(cityDateEl);
+    $(weatherCard).append(weathericon)
+    $(weatherCard).append(tempEl);
+    $(weatherCard).append(humidityEl);
+    $(weatherCard).append(windspeedEl);
+    $("#current-day-forecast").append(weatherCard);
+};
+
+// This is the line i changed
+// it used to be
+// $(.hist-button).click(function(){
+// ---code went here---
+// })
+// #city-list is the parent id --- button is the child element that
+// the click function will delegate to.
+$("#city-list").on("click", "button", function () {
+    // event.preventDefault();
+    $("#current-day-forecast").empty();
+    $("#five-day-forecast").empty();
+    cityName = $(this).text();
+    queryURLHist = QueryUrlHist();
+    $.ajax({
+        url: queryURLHist,
+        method: "GET"
+    }).then(function (data) {
+
+// Current Day Card ------------------------------------------------
+        weatherData = data;
+
+// current weather card
+        currentWeatherIcon = data.weather[0].icon;
+        date = moment().format("MMM Do YY");
+        currentWeatherIconEl = "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png";
+        weathericon = $("<img/>", {
+            id: "weather-icon",
+            src: currentWeatherIconEl,
+            width: 75
+        });
+        currentTemp = Math.floor(weatherData.main.temp);
+        weatherCard = $("<div>").addClass("card weather-card current-day-weather");
+        cityDateEl = $("<h5>").addClass("card-title").text(weatherData.name + " " + "(" + date + ")");
+        tempEl = $("<p>").addClass("card-text").text("Temp: " + currentTemp + " F");
+        humidityEl = $("<p>").addClass("card-text text-nowrap").text("Humidity: " + weatherData.main.humidity + " %");
+        windspeedEl = $("<p>").addClass("card-text").text("Windspeed: " + weatherData.wind.speed + " mph");
+        var uvIndexEl;
+        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=0d2a570544db7d02e47387057bd868ca"
+        CurrentWeatherCardHist();
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            uvIndexEl = response.value;
+            uvIndexTag = $("<p>").text("UV Index: " + uvIndexEl);
+            $(".current-day-weather").append(uvIndexTag);
+        })
+
+// Five Day Forecast ---------------------------------------------------------
+        function FiveDayQueryUrlHist() {
+            var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+            var fiveDayQueryParams = { "appid": "0d2a570544db7d02e47387057bd868ca" };
+            fiveDayQueryParams.id = data.id;
+            fiveDayQueryParams.units = "imperial";
+            return fiveDayQueryURL + $.param(fiveDayQueryParams);
+        }
+        fiveDayQueryURL = FiveDayQueryUrlHist();
+        $.ajax({
+            url: fiveDayQueryURL,
+            method: "GET"
+        }).then(function (fiveData) {
+            fiveDayList = fiveData.list;
+            for (var i = 4; i < fiveDayList.length; i += 8) {
+                var day = fiveDayList[i];
+                var dayIcon = day.weather[0].icon;
+                var dayWeatherIcon = "https://openweathermap.org/img/wn/" + dayIcon + ".png";
+                var dayIconEl = $("<img/>", {
+                    id: "weather-icon",
+                    src: dayWeatherIcon,
+                    width: 50
+                })
+                var dayTempEl = Math.floor(day.main.temp);
+                var dayCard = $("<div>").addClass("card weather-card col-lg bg-info text-white mr-md-2 mb-3");
+                var dayDate = $("<h5>").attr("style", "font-size:100%").addClass("card-title text-nowrap").text(moment().add(1, 'days').format('L'));
+                var dayTemp = $("<p>").addClass("card-text").text("Temp: " + dayTempEl + " F");
+                var dayHum = $("<p>").addClass("card-text text-nowrap").text("Humidity: " + day.main.humidity);
+                $(dayCard).append(dayDate);
+                $(dayCard).append(dayIconEl);
+                $(dayCard).append(dayTemp);
+                $(dayCard).append(dayHum);
+                $("#five-day-forecast").append(dayCard);
+            }
+        })
+    })
+})
